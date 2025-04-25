05156ca214889c5d934e66a056ab9d617e0aa994
Andrey Vasilyev <vasilyev.a@mindbox.cloud>
2025-04-25 11:02:13 +0300

    add migration

---
+++ Infrastructure/Migrations/20250424110437_Initial.cs
@@ -0,0 +1,247 @@
+﻿using System.Collections.Generic;
+using Microsoft.EntityFrameworkCore.Migrations;
+using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
+
+#nullable disable
+
+namespace Infrastructure.Migrations
+{
+    /// <inheritdoc />
+    public partial class Initial : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterDatabase()
+                .Annotation("Npgsql:PostgresExtension:hstore", ",,");
+
+            migrationBuilder.CreateTable(
+                name: "Users",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "integer", nullable: false)
+                        .Annotation("Npgsql:ValueGenerationStrategy", NpgsqlValueGenerationStrategy.IdentityByDefaultColumn),
+                    FirstName = table.Column<string>(type: "text", nullable: false),
+                    LastName = table.Column<string>(type: "text", nullable: false),
+                    Email = table.Column<string>(type: "text", nullable: false),
+                    PasswordHash = table.Column<string>(type: "text", nullable: false),
+                    PasswordSalt = table.Column<byte[]>(type: "bytea", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Users", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Workspace",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "integer", nullable: false)
+                        .Annotation("Npgsql:ValueGenerationStrategy", NpgsqlValueGenerationStrategy.IdentityByDefaultColumn),
+                    Name = table.Column<string>(type: "text", nullable: false),
+                    OwnerId = table.Column<int>(type: "integer", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Workspace", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_Workspace_Users_OwnerId",
+                        column: x => x.OwnerId,
+                        principalTable: "Users",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "SecurityGroup",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "integer", nullable: false)
+                        .Annotation("Npgsql:ValueGenerationStrategy", NpgsqlValueGenerationStrategy.IdentityByDefaultColumn),
+                    Name = table.Column<string>(type: "text", nullable: false),
+                    AllowedIps = table.Column<List<string>>(type: "text[]", nullable: false),
+                    WorkspaceId = table.Column<int>(type: "integer", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_SecurityGroup", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_SecurityGroup_Workspace_WorkspaceId",
+                        column: x => x.WorkspaceId,
+                        principalTable: "Workspace",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "WorkspaceUser",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "integer", nullable: false)
+                        .Annotation("Npgsql:ValueGenerationStrategy", NpgsqlValueGenerationStrategy.IdentityByDefaultColumn),
+                    UserId = table.Column<int>(type: "integer", nullable: false),
+                    WorkspaceId = table.Column<int>(type: "integer", nullable: false),
+                    Role = table.Column<string>(type: "text", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_WorkspaceUser", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_WorkspaceUser_Users_UserId",
+                        column: x => x.UserId,
+                        principalTable: "Users",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "FK_WorkspaceUser_Workspace_WorkspaceId",
+                        column: x => x.WorkspaceId,
+                        principalTable: "Workspace",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Clusters",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "integer", nullable: false)
+                        .Annotation("Npgsql:ValueGenerationStrategy", NpgsqlValueGenerationStrategy.IdentityByDefaultColumn),
+                    SystemName = table.Column<string>(type: "text", nullable: false),
+                    Status = table.Column<string>(type: "text", nullable: false),
+                    WorkspaceId = table.Column<int>(type: "integer", nullable: false),
+                    SecurityGroupId = table.Column<int>(type: "integer", nullable: true),
+                    ClusterNameInKubernetes = table.Column<string>(type: "text", nullable: false),
+                    RecoveryFromBackup = table.Column<bool>(type: "boolean", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Clusters", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_Clusters_SecurityGroup_SecurityGroupId",
+                        column: x => x.SecurityGroupId,
+                        principalTable: "SecurityGroup",
+                        principalColumn: "Id");
+                    table.ForeignKey(
+                        name: "FK_Clusters_Workspace_WorkspaceId",
+                        column: x => x.WorkspaceId,
+                        principalTable: "Workspace",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "ClusterConfigurations",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "integer", nullable: false)
+                        .Annotation("Npgsql:ValueGenerationStrategy", NpgsqlValueGenerationStrategy.IdentityByDefaultColumn),
+                    ClusterId = table.Column<int>(type: "integer", nullable: false),
+                    StorageSize = table.Column<int>(type: "integer", nullable: false),
+                    Cpu = table.Column<int>(type: "integer", nullable: false),
+                    Memory = table.Column<int>(type: "integer", nullable: false),
+                    MajorVersion = table.Column<int>(type: "integer", nullable: false),
+                    DatabaseName = table.Column<string>(type: "text", nullable: false),
+                    LcCollate = table.Column<string>(type: "text", nullable: false),
+                    LcCtype = table.Column<string>(type: "text", nullable: false),
+                    Instances = table.Column<int>(type: "integer", nullable: false),
+                    OwnerName = table.Column<string>(type: "text", nullable: false),
+                    OwnerPassword = table.Column<string>(type: "text", nullable: false),
+                    Parameters = table.Column<Dictionary<string, string>>(type: "hstore", nullable: true),
+                    PoolerMode = table.Column<string>(type: "text", nullable: true),
+                    PoolerMaxConnections = table.Column<int>(type: "integer", nullable: true),
+                    PoolerDefaultPoolSize = table.Column<int>(type: "integer", nullable: true),
+                    BackupScheduleCronExpression = table.Column<string>(type: "text", nullable: true),
+                    BackupMethod = table.Column<string>(type: "text", nullable: true),
+                    SyncReplicas = table.Column<int>(type: "integer", nullable: true),
+                    DataDurability = table.Column<string>(type: "text", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_ClusterConfigurations", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_ClusterConfigurations_Clusters_ClusterId",
+                        column: x => x.ClusterId,
+                        principalTable: "Clusters",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_ClusterConfigurations_ClusterId",
+                table: "ClusterConfigurations",
+                column: "ClusterId",
+                unique: true);
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Clusters_SecurityGroupId",
+                table: "Clusters",
+                column: "SecurityGroupId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Clusters_SystemName",
+                table: "Clusters",
+                column: "SystemName",
+                unique: true);
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Clusters_WorkspaceId",
+                table: "Clusters",
+                column: "WorkspaceId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_SecurityGroup_WorkspaceId",
+                table: "SecurityGroup",
+                column: "WorkspaceId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Users_Email",
+                table: "Users",
+                column: "Email",
+                unique: true);
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Workspace_Name",
+                table: "Workspace",
+                column: "Name",
+                unique: true);
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Workspace_OwnerId",
+                table: "Workspace",
+                column: "OwnerId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_WorkspaceUser_UserId_WorkspaceId",
+                table: "WorkspaceUser",
+                columns: new[] { "UserId", "WorkspaceId" },
+                unique: true);
+
+            migrationBuilder.CreateIndex(
+                name: "IX_WorkspaceUser_WorkspaceId",
+                table: "WorkspaceUser",
+                column: "WorkspaceId");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "ClusterConfigurations");
+
+            migrationBuilder.DropTable(
+                name: "WorkspaceUser");
+
+            migrationBuilder.DropTable(
+                name: "Clusters");
+
+            migrationBuilder.DropTable(
+                name: "SecurityGroup");
+
+            migrationBuilder.DropTable(
+                name: "Workspace");
+
+            migrationBuilder.DropTable(
+                name: "Users");
+        }
+    }
+}
